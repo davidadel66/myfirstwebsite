57491e9ce531b39a2bd5b77d05d4866639052e45
David Rezkalla <Davidadel66@gmail.com>
2022-12-17 23:26:58 -0500

    12/17

--- contact.php
+++ contact.php
@@ -1,18 +1,57 @@
+<!DOCTYPE html>
 <?php
+echo "<pre>";
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['name'];
+    print_r($_POST);
 
-    $mailTo = "davidadel66@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name. ".\n\n".$message;
+echo '</pre>';
+?>
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="./contact_2.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="app.js"></script>
 
- 
-    mail($mailTO, $subject, $txt, $headers);
+</head>
+<body>
+    <div id="myMenu" class="dropdown">
+        <a href="javascript: void(0)" class="buttonX" onclick="closeMenu()">&times;</a>
+    <div class="dropdown-lists">
+        <a href="./home.html">home.</a>
+        <a href="./contact_2.html">contact.</a>
+        <a href="./loki.html">loki.</a>
+    </div>
+    </div>
+    
 
-    header("Location: index.php?mailsend");
-}
+    <span class="hamburger" onclick="openMenu()">&#9776;</span>
+
+    
+    <div class="container">
+        <form action="contact.php" method="POST" class="form">
+            <div class="form-group">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Full Name" tabindex="1" required>
+            </div>
+            <div class="form-group">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" tabindex="2" required>
+            </div>
+            <div class="form-group">
+                <label for="subject" class="form-label">Subject</label>
+                <input type="subject" class="form-control" id="subject" name="subject" placeholder="Subject Title" tabindex="3" required>
+            </div>
+            <div class="form-group">
+                <label for="" class="form-label">Message</label>
+                <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter message" tabindex="4"></textarea>
+                <button type="submit" class="btn"> Send message!</button>
+            </div>
+        </form>
+    </div> 
+</body>
+</html>
---
+++ index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("myMenu").style.height = "100%"
+}
+
+function closeMenu() {
+    document.getElementById("myMenu").style.height = "0%"
+}
+
