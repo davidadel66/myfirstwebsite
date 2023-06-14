bdc79de294146139285187d8e618d21b11357db2
davidadel66 <davidadel66@gmail.com>
2023-06-14 19:52:22 -0400

    update

--- contact.js
+++ contact.js
@@ -5,18 +5,3 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("myMenu").style.height = "0%"
 }
-
-
-$(function() {
-    $(".form-control").on('focus', function(){
-        $(this).parents(".form-group").addClass('focused');
-    })
-
-    $(".form-control").on('blur', function(){
-        $(this).parents(".form-group").removeClass('focused');
-    })
-});
-
-
-
-
