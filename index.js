93e531762b656cbd98b0161332e348ac5cc32d77
davidadel66 <davidadel66@gmail.com>
2023-06-15 12:24:59 -0400

    update

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function closeMenu() {
     document.getElementById("myMenu").style.height = "0%"
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    const elements = document.querySelectorAll(".box");
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+  });
+  
